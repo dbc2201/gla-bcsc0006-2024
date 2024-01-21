2c23a585cb52d294b4ccb4da160c3e8930e5c866
dbc2201 <divyanshb22@gmail.com>
2024-01-21 19:24:08 +0530

    added constructor for the Node class

--- src/main/java/list/Node.java
+++ src/main/java/list/Node.java
@@ -1,6 +1,15 @@
 package list;
 
 public class Node {
-	private int data;
+	private final int data;
 	private Node nextNode;
+	
+	/**
+	 * Creates a new node will a 'null' next field.
+	 *
+	 * @param data the data to be stored in the node.
+	 */
+	public Node(int data) {
+		this.data = data;
+	}
 }
