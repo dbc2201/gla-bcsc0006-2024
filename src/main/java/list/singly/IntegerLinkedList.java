d5c450aff2c1c3b39c80f6cd4d70c1819aac3ba3
dbc2201 <divyanshb22@gmail.com>
2024-01-21 19:50:30 +0530

    moved classes to package

---
+++ src/main/java/list/singly/IntegerLinkedList.java
@@ -0,0 +1,13 @@
+package list.singly;
+
+import list.singly.Node;
+
+public class IntegerLinkedList {
+	private Node head;
+	private int size;
+	
+	public IntegerLinkedList() {
+		this.head = null;
+		this.size = 0;
+	}
+}
