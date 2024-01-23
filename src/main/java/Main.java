ddca27b2e29c5cff5fb160cffa1ba81fb041956b
dbc2201 <divyanshb22@gmail.com>
2024-01-23 18:33:03 +0530

    added notes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import list.singly.IntegerLinkedList;
+import list.singly.Node;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		IntegerLinkedList list = new IntegerLinkedList();
+		list.print();
+		list.add(45);
+		list.add(46);
+		list.add(47);
+		list.add(48);
+		list.add(49);
+		list.print();
+		Node searched = list.search(45);
 	}
 }
