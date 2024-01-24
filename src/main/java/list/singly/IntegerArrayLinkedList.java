373f910088fea94c565dde810dde359ad23317ce
dbc2201 <divyanshb22@gmail.com>
2024-01-24 18:42:20 +0530

    created IntegerArrayLinkedList.java

---
+++ src/main/java/list/singly/IntegerArrayLinkedList.java
@@ -0,0 +1,33 @@
+package list.singly;
+
+public class IntegerArrayLinkedList implements SinglyLinkedList {
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	@Override
+	public int getSize() {
+		return 0;
+	}
+	
+	@Override
+	public void add(int data) {
+	
+	}
+	
+	@Override
+	public int remove(int data) {
+		return 0;
+	}
+	
+	@Override
+	public Node search(int data) {
+		return null;
+	}
+	
+	@Override
+	public void print() {
+	
+	}
+}
