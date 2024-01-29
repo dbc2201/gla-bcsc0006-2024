cd329ddb0330af42c84995064dccac5150080d4c
dbc2201 <divyanshb22@gmail.com>
2024-01-30 00:48:55 +0530

    add methods in the DoublyLinkedList adt

--- src/main/java/list/doubly/adt/DoublyLinkedList.java
+++ src/main/java/list/doubly/adt/DoublyLinkedList.java
@@ -1,4 +1,16 @@
 package list.doubly.adt;
 
+import list.doubly.node.DoublyNode;
+
 public interface DoublyLinkedList {
+	boolean isEmpty();
+	int getSize();
+	
+	void add(int data);
+	
+	int remove(int data);
+	
+	DoublyNode search(int data);
+	
+	void print();
 }
