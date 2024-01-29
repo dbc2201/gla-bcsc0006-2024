20cb86932af24f8e2bbc4c60b1b210cab282bfc2
dbc2201 <divyanshb22@gmail.com>
2024-01-30 00:50:33 +0530

    add fields for the DoublyLinkedList class

--- src/main/java/list/doubly/list/IntegerDoublyLinkedList.java
+++ src/main/java/list/doubly/list/IntegerDoublyLinkedList.java
@@ -4,6 +4,10 @@ import list.doubly.adt.DoublyLinkedList;
 import list.doubly.node.DoublyNode;
 
 public class IntegerDoublyLinkedList implements DoublyLinkedList {
+	private int size;
+	public DoublyNode head;
+	public DoublyNode tail;
+	
 	@Override
 	public boolean isEmpty() {
 		return false;
