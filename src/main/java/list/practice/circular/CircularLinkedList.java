05532297fdd05330714ccef9ee172d63c84039cd
dbc2201 <divyanshb22@gmail.com>
2024-02-09 16:47:25 +0530

    add node class in the circular linked list

--- src/main/java/list/practice/circular/CircularLinkedList.java
+++ src/main/java/list/practice/circular/CircularLinkedList.java
@@ -1,4 +1,12 @@
 package list.practice.circular;
 
 public class CircularLinkedList {
+	static class Node {
+		int data;
+		Node next;
+		Node previous;
+		public Node(int data) {
+			this.data = data;
+		}
+	}
 }
