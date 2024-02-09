ef1f0f8871d4afc11c736698503ebc3d36410d53
dbc2201 <divyanshb22@gmail.com>
2024-02-09 16:45:55 +0530

    add node class definition inside the doubly linked list class

--- src/main/java/list/practice/doubly/DoublyLinkedList.java
+++ src/main/java/list/practice/doubly/DoublyLinkedList.java
@@ -1,4 +1,13 @@
 package list.practice.doubly;
 
 public class DoublyLinkedList {
+	static class Node {
+		int data;
+		Node next;
+		Node previous;
+		
+		public Node(int data) {
+			this.data = data;
+		}
+	}
 }
