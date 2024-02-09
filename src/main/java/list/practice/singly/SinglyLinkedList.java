9b7e94d161073170704116ff073e5b57875ca1b3
dbc2201 <divyanshb22@gmail.com>
2024-02-09 16:43:16 +0530

    create basic structure of a singly linked list that includes the node class, head and size

--- src/main/java/list/practice/singly/SinglyLinkedList.java
+++ src/main/java/list/practice/singly/SinglyLinkedList.java
@@ -1,4 +1,15 @@
 package list.practice.singly;
 
 public class SinglyLinkedList {
+	Node head;
+	int size;
+	
+	static class Node {
+		int data;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+		}
+	}
 }
