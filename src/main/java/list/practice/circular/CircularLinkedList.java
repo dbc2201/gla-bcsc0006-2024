7b5cabc9e5072c7e0e4e0dd814943483260fa0bd
dbc2201 <divyanshb22@gmail.com>
2024-02-09 16:47:44 +0530

    create basic structure for a circular linked list

--- src/main/java/list/practice/circular/CircularLinkedList.java
+++ src/main/java/list/practice/circular/CircularLinkedList.java
@@ -1,10 +1,14 @@
 package list.practice.circular;
 
 public class CircularLinkedList {
+	Node head;
+	Node tail;
+	int size;
 	static class Node {
 		int data;
 		Node next;
 		Node previous;
+		
 		public Node(int data) {
 			this.data = data;
 		}
