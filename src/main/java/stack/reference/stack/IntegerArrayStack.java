78e1d92cbb341ed2addd51c8b893715503b83cd8
dbc2201 <divyanshb22@gmail.com>
2024-02-12 22:33:46 +0530

    add fields for an integer stack and initialize them in the constructor

--- src/main/java/stack/reference/stack/IntegerArrayStack.java
+++ src/main/java/stack/reference/stack/IntegerArrayStack.java
@@ -3,6 +3,15 @@ package stack.reference.stack;
 import stack.reference.adt.Stack;
 
 public class IntegerArrayStack implements Stack {
+	private final int[] array;
+	int top;
+	int size;
+	
+	public IntegerArrayStack(int capacity) {
+		this.array = new int[capacity];
+		this.top = -1;
+		this.size = 0;
+	}
 	
 	@Override
 	public void push(int data) {
