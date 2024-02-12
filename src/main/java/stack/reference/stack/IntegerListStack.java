183a897d840dd02bcd795575f6b8b930da8f4478
dbc2201 <divyanshb22@gmail.com>
2024-02-12 22:45:14 +0530

    implement the methods of the stack adt

--- src/main/java/stack/reference/stack/IntegerListStack.java
+++ src/main/java/stack/reference/stack/IntegerListStack.java
@@ -4,5 +4,29 @@ import stack.reference.adt.Stack;
 import stack.reference.node.StackNode;
 
 public class IntegerListStack implements Stack {
-
+	
+	@Override
+	public void push(int data) {
+	
+	}
+	
+	@Override
+	public int pop() {
+		return 0;
+	}
+	
+	@Override
+	public int peek() {
+		return 0;
+	}
+	
+	@Override
+	public boolean isUnderflow() {
+		return false;
+	}
+	
+	@Override
+	public int getSize() {
+		return 0;
+	}
 }
