87511563e389f2f12dade1aaf893ced3f08bc559
dbc2201 <divyanshb22@gmail.com>
2024-02-12 22:32:26 +0530

    implement methods from the stack adt

--- src/main/java/stack/reference/stack/IntegerArrayStack.java
+++ src/main/java/stack/reference/stack/IntegerArrayStack.java
@@ -3,5 +3,29 @@ package stack.reference.stack;
 import stack.reference.adt.Stack;
 
 public class IntegerArrayStack implements Stack {
-
+	
+	@Override
+	public void push(int data) {
+	
+	}
+	
+	@Override
+	public int pop() {
+		return 0;
+	}
+	
+	@Override
+	public int peek() {
+		return 0;
+	}
+	
+	@Override
+	public boolean isUnderflow() {
+		return false;
+	}
+	
+	@Override
+	public int getSize() {
+		return 0;
+	}
 }
