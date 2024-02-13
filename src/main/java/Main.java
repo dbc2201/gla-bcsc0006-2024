a3ffc9e0b9fdf19d5da4bd23475cceba43774dce
dbc2201 <divyanshb22@gmail.com>
2024-02-13 20:56:11 +0530

    remove code from main()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
-import stack.reference.stack.IntegerArrayStack;
-import stack.reference.stack.IntegerListStack;
-
 public class Main {
 	public static void main(String[] args) {
-		IntegerArrayStack arrayStack = new IntegerArrayStack(10);
-		arrayStack.push(45);
-		arrayStack.push(46);
-		arrayStack.push(47);
-		arrayStack.push(48);
-		arrayStack.push(49);
-		arrayStack.push(50);
+	
 	}
 }
