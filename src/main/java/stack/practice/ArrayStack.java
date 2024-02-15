e92bada22f009fe7a4b640de80705c9d1e39710c
dbc2201 <divyanshb22@gmail.com>
2024-02-15 20:42:42 +0530

    add methods for stack

--- src/main/java/stack/practice/ArrayStack.java
+++ src/main/java/stack/practice/ArrayStack.java
@@ -1,5 +1,15 @@
 package stack.practice;
 
 public class ArrayStack {
-
+	public void push(int data) {
+	
+	}
+	
+	public int pop() {
+		return Integer.MIN_VALUE;
+	}
+	
+	public int peek() {
+		return Integer.MIN_VALUE;
+	}
 }
