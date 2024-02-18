22d431188aba7e7c092c5f8f107638a1bc1fb678
dbc2201 <divyanshb22@gmail.com>
2024-02-19 01:20:18 +0530

    add code for QueueNode

--- src/main/java/queue/reference/node/QueueNode.java
+++ src/main/java/queue/reference/node/QueueNode.java
@@ -1,5 +1,15 @@
 package queue.reference.node;
 
 public class QueueNode {
-
+	int data;
+	QueueNode next;
+	
+	public QueueNode(int data) {
+		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "QueueNode{" + "data=" + data + ", next=" + next + '}';
+	}
 }
