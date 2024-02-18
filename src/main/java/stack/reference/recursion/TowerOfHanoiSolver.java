bc67e83da672641abdfbb4492d90833a35b9fb7a
dbc2201 <divyanshb22@gmail.com>
2024-02-18 14:26:41 +0530

    add code to show steps to solve the tower of hanoi problem

--- src/main/java/stack/reference/recursion/TowerOfHanoiSolver.java
+++ src/main/java/stack/reference/recursion/TowerOfHanoiSolver.java
@@ -1,4 +1,13 @@
 package stack.reference.recursion;
 
 public class TowerOfHanoiSolver {
+	void showSteps(int numberOfTowers, char startingTower, char endingTower, char helperTower) {
+		if (numberOfTowers == 1) {
+			System.out.println("Take disk from " + startingTower + " to " + endingTower);
+			return;
+		}
+		showSteps(numberOfTowers - 1, startingTower, helperTower, endingTower);
+		System.out.println("Take disk from " + startingTower + " to " + endingTower);
+		showSteps(numberOfTowers - 1, helperTower, endingTower, startingTower);
+	}
 }
