8709b87bffd3f106bf4ad5d83fcbb66006e39d6c
dbc2201 <divyanshb22@gmail.com>
2024-02-19 01:23:06 +0530

    add methods for a queue implemented using a linked list

--- src/main/java/queue/reference/queue/singly/IntegerListQueue.java
+++ src/main/java/queue/reference/queue/singly/IntegerListQueue.java
@@ -1,4 +1,38 @@
 package queue.reference.queue.singly;
 
-public class IntegerListQueue {
+import queue.reference.adt.Queue;
+import queue.reference.node.QueueNode;
+
+public class IntegerListQueue implements Queue {
+	QueueNode front;
+	QueueNode rear;
+	int size;
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@Override
+	public void offer(int data) {
+	
+	}
+	
+	@Override
+	public int remove() {
+		return 0;
+	}
+	
+	@Override
+	public int poll() {
+		return 0;
+	}
+	
+	@Override
+	public int peek() {
+		return 0;
+	}
 }
