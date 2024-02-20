e750e62c0aa1c34b80d281606ee84ac05222dc45
dbc2201 <divyanshb22@gmail.com>
2024-02-21 04:05:14 +0530

    add fields for the DoubleEndedQueue.java

--- src/main/java/queue/reference/queue/deque/DoubleEndedQueue.java
+++ src/main/java/queue/reference/queue/deque/DoubleEndedQueue.java
@@ -1,4 +1,9 @@
 package queue.reference.queue.deque;
 
+import queue.reference.node.QueueNode;
+
 public class DoubleEndedQueue {
+	public QueueNode front;
+	public QueueNode rear;
+	private int size;
 }
