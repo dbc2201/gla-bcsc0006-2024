b37cc858fbdc9f97deddb8245da9c9dd6aebe0ca
dbc2201 <divyanshb22@gmail.com>
2024-02-21 04:04:02 +0530

    add method templates

--- src/main/java/queue/practice/queue/Queue.java
+++ src/main/java/queue/practice/queue/Queue.java
@@ -1,6 +1,30 @@
 package queue.practice.queue;
 
 public class Queue {
+	Node front;
+	Node rear;
+	int size;
+	
+	public int getSize() {
+		return Integer.MIN_VALUE;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public void enqueue(int data) {
+		return;
+	}
+	
+	public int dequeue() {
+		return Integer.MIN_VALUE;
+	}
+	
+	public int peek() {
+		return Integer.MIN_VALUE;
+	}
+	
 	static class Node {
 		int data;
 		Node next;
