85dc7c9848302776fa5826ef9ce06c30cc7e98cd
dbc2201 <divyanshb22@gmail.com>
2024-02-21 04:02:53 +0530

    add a static inner class for node

--- src/main/java/queue/practice/queue/Queue.java
+++ src/main/java/queue/practice/queue/Queue.java
@@ -1,4 +1,17 @@
 package queue.practice.queue;
 
 public class Queue {
+	static class Node {
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+		}
+		
+		@Override
+		public String toString() {
+			return "Node{" + "data=" + data + ", next=" + next + '}';
+		}
+	}
 }
