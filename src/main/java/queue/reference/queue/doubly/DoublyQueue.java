02670996609fbf3cc24ef6b3de3872bfa0c29e4f
dbc2201 <divyanshb22@gmail.com>
2024-02-21 04:27:36 +0530

    complete the definition of the DoublyQueue class

--- src/main/java/queue/reference/queue/doubly/DoublyQueue.java
+++ src/main/java/queue/reference/queue/doubly/DoublyQueue.java
@@ -1,4 +1,47 @@
 package queue.reference.queue.doubly;
 
 public class DoublyQueue {
+	public DoublyQueueNode front;
+	public DoublyQueueNode rear;
+	int size;
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void enqueue(int data) {
+		DoublyQueueNode node = new DoublyQueueNode(data);
+		if (isEmpty()) {
+			front = rear = node;
+		} else {
+			rear.next = node;
+			node.previous = rear;
+			rear = node;
+		}
+		size++;
+	}
+	
+	public int dequeue() {
+		if (isEmpty()) {
+			System.err.println("Queue is empty!");
+			return Integer.MIN_VALUE;
+		}
+		int removed = front.data;
+		front = front.next;
+		front.previous = null;
+		size--;
+		return removed;
+	}
+	
+	public int peek() {
+		if (isEmpty()) {
+			System.err.println("Queue is empty!");
+			return Integer.MIN_VALUE;
+		}
+		return front.data;
+	}
 }
