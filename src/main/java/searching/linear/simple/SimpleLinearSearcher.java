455f8bf86e0922ea80e652cf4299ee019c510db9
dbc2201 <divyanshb22@gmail.com>
2024-03-11 19:28:20 +0530

    add method to perform linear search

--- src/main/java/searching/linear/simple/SimpleLinearSearcher.java
+++ src/main/java/searching/linear/simple/SimpleLinearSearcher.java
@@ -1,4 +1,14 @@
 package searching.linear.simple;
 
 public class SimpleLinearSearcher {
+	/**
+	 * This method performs a linear search in the numbers array to find the key.
+	 *
+	 * @param numbers the array to be searched.
+	 * @param key     the value to be found.
+	 * @return -1 if the key is not found in the array, otherwise the index at which the key was found.
+	 */
+	public int linearSearch(int[] numbers, int key) {
+		return Integer.MIN_VALUE;
+	}
 }
