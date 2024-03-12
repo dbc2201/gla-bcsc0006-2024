ad49a1965e7135c2ebed55e337b88e6ae195e77e
dbc2201 <divyanshb22@gmail.com>
2024-03-12 14:16:54 +0530

    add constructors

--- src/main/java/searching/linear/simple/student/Student.java
+++ src/main/java/searching/linear/simple/student/Student.java
@@ -3,5 +3,15 @@ package searching.linear.simple.student;
 public class Student {
 	private int rollNumber;
 	private String name;
+	
+	public Student() {
+		this.rollNumber = 4;
+		this.name = "DBC";
+	}
+	
+	public Student(int rollNumber, String name) {
+		this.rollNumber = rollNumber;
+		this.name = name;
+	}
 }
 
