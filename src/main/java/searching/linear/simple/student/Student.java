c279f640ee1efc66e9e488c61e55f11fd2662012
dbc2201 <divyanshb22@gmail.com>
2024-03-12 14:17:11 +0530

    add getters and setters for the fields

--- src/main/java/searching/linear/simple/student/Student.java
+++ src/main/java/searching/linear/simple/student/Student.java
@@ -13,5 +13,21 @@ public class Student {
 		this.rollNumber = rollNumber;
 		this.name = name;
 	}
+	
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
 
