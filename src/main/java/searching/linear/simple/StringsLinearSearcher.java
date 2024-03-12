e562d399bc14431a56cc57a745f1cb9faa6625ef
dbc2201 <divyanshb22@gmail.com>
2024-03-12 14:13:26 +0530

    add method and javadoc to search a name

--- src/main/java/searching/linear/simple/StringsLinearSearcher.java
+++ src/main/java/searching/linear/simple/StringsLinearSearcher.java
@@ -1,5 +1,15 @@
 package searching.linear.simple;
 
 public class StringsLinearSearcher {
-
+	/**
+	 * This method performs a linear search in the names array to find the key.
+	 *
+	 * @param names an array of Strings that contains some (non-null) names.
+	 * @param key   a name that is to be searched in the array.
+	 * @return -1 if the name is not found in the array, otherwise the index at which the name is found.
+	 */
+	public int linearSearch(String[] names, String key) {
+		// TODO add your linear search algorithm here after deleting this line.
+		return Integer.MIN_VALUE;
+	}
 }
