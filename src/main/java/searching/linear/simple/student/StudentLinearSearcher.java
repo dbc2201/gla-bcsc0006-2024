b2a3181cfd72101338f3d6b08b64f69c43287a55
dbc2201 <divyanshb22@gmail.com>
2024-03-17 12:49:05 +0530

    complete the StudentLinearSearcher

---
+++ src/main/java/searching/linear/simple/student/StudentLinearSearcher.java
@@ -0,0 +1,14 @@
+package searching.linear.simple.student;
+
+public class StudentLinearSearcher {
+	/**
+	 * This method performs a linear search in the students array to find the key.
+	 *
+	 * @param students the array to be searched.
+	 * @param key      the value to be found.
+	 * @return -1 if the key is not found in the array, otherwise the index at which the key was found.
+	 */
+	public int search(Student[] students, Student key) {
+		return Integer.MIN_VALUE;
+	}
+}
