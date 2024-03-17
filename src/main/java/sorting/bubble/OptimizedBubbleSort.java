d9a502d525e4f44f6d08fad62a119cb1e2e6315d
dbc2201 <divyanshb22@gmail.com>
2024-03-17 12:57:42 +0530

    fix broken code

--- src/main/java/sorting/bubble/OptimizedBubbleSort.java
+++ src/main/java/sorting/bubble/OptimizedBubbleSort.java
@@ -2,18 +2,13 @@ package sorting.bubble;
 
 public class OptimizedBubbleSort {
 	public void sort(int[] numbers) {
-		boolean isSorted = false;
 		for (int counter = 0; counter < numbers.length; counter++) {
 			for (int index = 0; index < numbers.length - counter - 1; index++) {
 				if (numbers[index] > numbers[index + 1]) {
-					isSorted = true;
 					int temp = numbers[index + 1];
 					numbers[index + 1] = numbers[index];
 					numbers[index] = temp;
 				}
-			}
-			if (isSorted) {
-				return;
 			}
 		}
 	}
