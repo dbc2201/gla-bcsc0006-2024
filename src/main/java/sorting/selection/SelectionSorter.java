64a7e38641791e21b195949c53acaeb5c8a7b734
dbc2201 <divyanshb22@gmail.com>
2024-03-17 13:23:44 +0530

    add code for selection sort

--- src/main/java/sorting/selection/SelectionSorter.java
+++ src/main/java/sorting/selection/SelectionSorter.java
@@ -2,6 +2,22 @@ package sorting.selection;
 
 public class SelectionSorter {
 	public void sort(int[] numbers) {
-	
+		// repeat (numOfElements - 1) times
+		for (int swapIndex = 0; swapIndex < numbers.length - 1; swapIndex++) {
+			// set the first unsorted element as the minimum
+			int minimumIndex = swapIndex;
+			// find the index of the minimum element in the unsorted portion
+			for (int index = swapIndex + 1; index < numbers.length; index++) {
+				// if element < current minimum
+				if (numbers[index] < numbers[minimumIndex]) {
+					// update the index of the minimum element
+					minimumIndex = index;
+				}
+			}
+			// swap the minimum element with the first unsorted position
+			int temp = numbers[swapIndex];
+			numbers[swapIndex] = numbers[minimumIndex];
+			numbers[minimumIndex] = temp;
+		}
 	}
 }
