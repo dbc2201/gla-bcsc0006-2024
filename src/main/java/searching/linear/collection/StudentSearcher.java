201b3b1dfb89827bba9ad88d586ac8662f98960f
dbc2201 <divyanshb22@gmail.com>
2024-03-17 12:51:14 +0530

    add javadoc

--- src/main/java/searching/linear/collection/StudentSearcher.java
+++ src/main/java/searching/linear/collection/StudentSearcher.java
@@ -5,6 +5,13 @@ import searching.linear.simple.student.Student;
 import java.util.List;
 
 public class StudentSearcher {
+	/**
+	 * This method performs a linear search in the students array to find the key.
+	 *
+	 * @param students the list to be searched.
+	 * @param key      the value to be found.
+	 * @return -1 if the key is not found in the array, otherwise the index at which the key was found.
+	 */
 	public int search(List<Student> students, Student key) {
 		return Integer.MIN_VALUE;
 	}
