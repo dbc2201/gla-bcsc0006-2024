e8ef6d328eedadd6cc8a12bb69295f0682dc2fe1
dbc2201 <divyanshb22@gmail.com>
2024-03-17 12:54:19 +0530

    complete code for Bubble Sort

--- src/main/java/sorting/bubble/BubbleSorter.java
+++ src/main/java/sorting/bubble/BubbleSorter.java
@@ -2,6 +2,14 @@ package sorting.bubble;
 
 public class BubbleSorter {
 	public void sort(int[] numbers) {
-	
+		for (int counter = 0; counter < numbers.length; counter++) {
+			for (int index = 0; index < (numbers.length - 1); index++) {
+				if (numbers[index] > numbers[index + 1]) {
+					int temp = numbers[index];
+					numbers[index] = numbers[index + 1];
+					numbers[index + 1] = temp;
+				}
+			}
+		}
 	}
 }
