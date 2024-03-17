041acc6e681602b1dc1a92e95eedef7d3b411523
dbc2201 <divyanshb22@gmail.com>
2024-03-17 12:50:30 +0530

    add method to search in a list of Student

--- src/main/java/searching/linear/collection/StudentSearcher.java
+++ src/main/java/searching/linear/collection/StudentSearcher.java
@@ -1,4 +1,11 @@
 package searching.linear.collection;
 
+import searching.linear.simple.student.Student;
+
+import java.util.List;
+
 public class StudentSearcher {
+	public int search(List<Student> students, Student key) {
+		return Integer.MIN_VALUE;
+	}
 }
