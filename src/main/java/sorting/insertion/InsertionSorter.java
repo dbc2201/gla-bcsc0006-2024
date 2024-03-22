cb725c4df330b21deac716bacf36ef7d062b0939
dbc2201 <divyanshb22@gmail.com>
2024-03-22 11:02:16 +0530

    add code for insertion sort

--- src/main/java/sorting/insertion/InsertionSorter.java
+++ src/main/java/sorting/insertion/InsertionSorter.java
@@ -2,6 +2,14 @@ package sorting.insertion;
 
 public class InsertionSorter {
 	public void sort(int[] numbers) {
-	
+		for (int index = 0; index < numbers.length; index++) {
+			int currentElement = numbers[index];
+			int comparingIndex = index - 1;
+			while (comparingIndex >= 0 && numbers[comparingIndex] > currentElement) {
+				numbers[comparingIndex + 1] = numbers[comparingIndex];
+				comparingIndex--;
+			}
+			numbers[comparingIndex + 1] = currentElement;
+		}
 	}
 }
