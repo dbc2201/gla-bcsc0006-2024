a46587a0f88ec8f5b31a1a879ae730244e55ac62
dbc2201 <divyanshb22@gmail.com>
2024-04-24 23:07:06 +0530

    complete Vertex class

--- src/main/java/graph/undirected/unweighted/Vertex.java
+++ src/main/java/graph/undirected/unweighted/Vertex.java
@@ -1,5 +1,36 @@
 package graph.undirected.unweighted;
 
-public class Vertex {
+import java.util.Objects;
 
+public class Vertex {
+	private String label;
+	
+	public Vertex(String label) {
+		this.label = label;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Vertex vertex)) return false;
+		return Objects.equals(getLabel(), vertex.getLabel());
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(getLabel());
+	}
+	
+	@Override
+	public String toString() {
+		return label;
+	}
 }
