e87e523212f3e2891b601ce29d715e9c9102e74b
dbc2201 <divyanshb22@gmail.com>
2024-04-27 22:57:46 +0530

    complete the edge class for the weighted graph

--- src/main/java/graph/undirected/weighted/Edge.java
+++ src/main/java/graph/undirected/weighted/Edge.java
@@ -1,5 +1,34 @@
 package graph.undirected.weighted;
 
-public class Edge {
+import java.util.Objects;
 
+public class Edge {
+	private final Vertex fromVertex;
+	private final Vertex toVertex;
+	private final double weight;
+	
+	public Edge(Vertex fromVertex, Vertex toVertex, double weight) {
+		this.fromVertex = fromVertex;
+		this.toVertex = toVertex;
+		this.weight = weight;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Edge edge)) return false;
+		return Double.compare(weight, edge.weight) == 0 &&
+				Objects.equals(fromVertex, edge.fromVertex) &&
+				Objects.equals(toVertex, edge.toVertex);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(fromVertex, toVertex, weight);
+	}
+	
+	@Override
+	public String toString() {
+		return fromVertex + " -- (" + weight + ") -- " + toVertex;
+	}
 }
