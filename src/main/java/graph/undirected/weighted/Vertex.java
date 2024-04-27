d2a1967d567afbd0a022527a44b9c2aa14ce3db9
dbc2201 <divyanshb22@gmail.com>
2024-04-27 22:55:04 +0530

    complete the vertex class for the weighted graph

--- src/main/java/graph/undirected/weighted/Vertex.java
+++ src/main/java/graph/undirected/weighted/Vertex.java
@@ -1,4 +1,28 @@
 package graph.undirected.weighted;
 
+import java.util.Objects;
+
 public class Vertex {
+	private final String label;
+	
+	public Vertex(String label) {
+		this.label = label;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Vertex vertex)) return false;
+		return Objects.equals(label, vertex.label);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(label);
+	}
+	
+	@Override
+	public String toString() {
+		return label;
+	}
 }
